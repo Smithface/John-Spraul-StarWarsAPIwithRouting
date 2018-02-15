24cd3ccf6626df4bea7830d5690eb0f254c4e7fd
Smithface <kylewibblesmith@gmail.com>
2018-02-15 16:47:08 -0500

    changed CharacterSheet to class
    it used to be a function, but I am going to give it a state and use that state to change which movie it renders

--- starwars/src/CharacterSheet.js
+++ starwars/src/CharacterSheet.js
@@ -2,32 +2,35 @@ import React from 'react';
 import './CharacterSheet.css';
 import pictures from './pictures';
 
-function CharacterSheet(props) {
-  return(
-    <div className="container">
-      <div className="container__topRow">        {/* top row */}
-        <div>Star Wars</div>
-        <div>Character License</div>
+class CharacterSheet extends React.Component {
+
+  render() {
+    return(
+      <div className="container">
+        <div className="container__topRow">        {/* top row */}
+          <div>Star Wars</div>
+          <div>Character License</div>
+        </div>
+        <div className="container__middleRow">        {/* middle row */}
+          <img src={pictures[this.props.index].picture} alt={this.props.charInfo.name} />
+          <div className="container__middleRow-right">          {/* center start */}
+            <div><b>Name:</b> {this.props.charInfo.name}</div>
+            <div><b>YOB:</b> {this.props.charInfo.birth_year}</div>
+            <div><b>Gender:</b> {this.props.charInfo.gender}</div>
+            <div><b>Eye Color:</b> {this.props.charInfo.eye_color}</div>
+            <div><b>Hair Color:</b> {this.props.charInfo.hair_color}</div>
+            <div><b>Skin:</b> {this.props.charInfo.skin_color}</div>
+            <div><b>Height:</b> {this.props.charInfo.height}</div>
+            <div><b>Mass:</b> {this.props.charInfo.mass}</div>
+          </div>          {/* center end */}
+        </div>
+        <div className="container__bottomRow">        {/* bottom row */}
+          <div><b>created:</b> {this.props.charInfo.created}</div>
+          <div><b>edited:</b> {this.props.charInfo.edited}</div>
+        </div>
       </div>
-      <div className="container__middleRow">        {/* middle row */}
-        <img src={pictures[props.index].picture} alt={props.charInfo.name} />
-        <div className="container__middleRow-right">          {/* center start */}
-          <div><b>Name:</b> {props.charInfo.name}</div>
-          <div><b>YOB:</b> {props.charInfo.birth_year}</div>
-          <div><b>Gender:</b> {props.charInfo.gender}</div>
-          <div><b>Eye Color:</b> {props.charInfo.eye_color}</div>
-          <div><b>Hair Color:</b> {props.charInfo.hair_color}</div>
-          <div><b>Skin:</b> {props.charInfo.skin_color}</div>
-          <div><b>Height:</b> {props.charInfo.height}</div>
-          <div><b>Mass:</b> {props.charInfo.mass}</div>
-        </div>          {/* center end */}
-      </div>
-      <div className="container__bottomRow">        {/* bottom row */}
-        <div><b>created:</b> {props.charInfo.created}</div>
-        <div><b>edited:</b> {props.charInfo.edited}</div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default CharacterSheet;
