985a844cecb819f1f47b0f5fcb5ae5a6bd1a6aa5
Smithface <kylewibblesmith@gmail.com>
2018-02-15 17:28:56 -0500

    finished refactoring
    the data is now brought in on componentDidMount() on the CharacterSheet Component in CharacterSheet.js

--- starwars/src/CharacterSheet.js
+++ starwars/src/CharacterSheet.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import './CharacterSheet.css';
 import pictures from './pictures';
 
@@ -12,11 +13,19 @@ class CharacterSheet extends React.Component {
     let id = null;
     if(this.props.index === undefined) {
       id = this.props.match.params.id
-    } else { id = this.props.index }
-    
+    } else { id = this.props.index + 1 }
+    axios
+      .get(`https://swapi.co/api/people/${id}/`)
+      .then(response => this.setState(() => ({ character: response}) ))
+      .catch(error => {console.error(error)});
   }
 
   render() {
+    if(!this.state.character) {
+      return <div>Loading character information...</div>
+    }
+
+    const { name, birth_year, gender, eye_color, hair_color, skin_color, height, mass, created, edited } = this.state.character.data;
     return(
       <div className="container">
         <div className="container__topRow">        {/* top row */}
@@ -24,21 +33,21 @@ class CharacterSheet extends React.Component {
           <div>Character License</div>
         </div>
         <div className="container__middleRow">        {/* middle row */}
-          <img src={pictures[this.props.index].picture} alt={this.props.charInfo.name} />
+          <img src={pictures[this.props.index].picture} alt={name} />
           <div className="container__middleRow-right">          {/* center start */}
-            <div><b>Name:</b> {this.props.charInfo.name}</div>
-            <div><b>YOB:</b> {this.props.charInfo.birth_year}</div>
-            <div><b>Gender:</b> {this.props.charInfo.gender}</div>
-            <div><b>Eye Color:</b> {this.props.charInfo.eye_color}</div>
-            <div><b>Hair Color:</b> {this.props.charInfo.hair_color}</div>
-            <div><b>Skin:</b> {this.props.charInfo.skin_color}</div>
-            <div><b>Height:</b> {this.props.charInfo.height}</div>
-            <div><b>Mass:</b> {this.props.charInfo.mass}</div>
+            <div><b>Name:</b> {name}</div>
+            <div><b>YOB:</b> {birth_year}</div>
+            <div><b>Gender:</b> {gender}</div>
+            <div><b>Eye Color:</b> {eye_color}</div>
+            <div><b>Hair Color:</b> {hair_color}</div>
+            <div><b>Skin:</b> {skin_color}</div>
+            <div><b>Height:</b> {height}</div>
+            <div><b>Mass:</b> {mass}</div>
           </div>          {/* center end */}
         </div>
         <div className="container__bottomRow">        {/* bottom row */}
-          <div><b>created:</b> {this.props.charInfo.created}</div>
-          <div><b>edited:</b> {this.props.charInfo.edited}</div>
+          <div><b>created:</b> {created}</div>
+          <div><b>edited:</b> {edited}</div>
         </div>
       </div>
     );
