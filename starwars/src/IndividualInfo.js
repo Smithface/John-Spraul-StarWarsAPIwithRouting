bc1c79db719e957b4d1897badee1475aa1a09219
Smithface <kylewibblesmith@gmail.com>
2018-02-15 18:53:06 -0500

    added big button to return to main page

--- starwars/src/IndividualInfo.js
+++ starwars/src/IndividualInfo.js
@@ -3,10 +3,12 @@ import './IndividualInfo.css'
 import CharacterSheet from './CharacterSheet';
 
 function IndividualInfo(props) {
-  console.log(props);
   return (
-    <div className="limitsheetsize">
-      <CharacterSheet index={Number(props.match.params.id - 1)} />
+    <div className="parentBox">
+      <div className="limitSheetSize">
+        <CharacterSheet index={Number(props.match.params.id - 1)} />
+      </div>
+      <button className="bigButton" onClick={ () => {props.history.push("/")} }>Back to the Main Page</button>
     </div>
   );
 }
