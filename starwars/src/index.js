ad6d1c74748601af902450b106e6d4926d82f30b
Smithface <kylewibblesmith@gmail.com>
2018-02-15 20:55:47 -0500

    imported BlankSlate into index
    time to test until it works

--- starwars/src/index.js
+++ starwars/src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
 import Characters from './Characters';
 import IndividualInfo from './IndividualInfo'
+import BlankSlate from './BlankSlate';
 
 const App = () => (
   <Router>
@@ -12,6 +13,7 @@ const App = () => (
         <h1 className="Header">React Wars</h1>
         <Route path = "/" component={Characters} exact />
         <Route path = "/character/:id" component={IndividualInfo} />
+        <Route path = "/:category/:number" component={BlankSlate} />
       </div>
     </div>
   </Router>
