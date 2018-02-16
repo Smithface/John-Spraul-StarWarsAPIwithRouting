c96f568244adbbdd74947622c84024ad8084cce2
Smithface <kylewibblesmith@gmail.com>
2018-02-15 20:52:15 -0500

    set outline for BlankSlate
    need to add dynamic links, but that will come later

--- starwars/src/BlankSlate.js
+++ starwars/src/BlankSlate.js
@@ -1,54 +1,48 @@
 import React from 'react';
 import axios from 'axios';
 import './BlankSlate.css';
-import pictures from './pictures';
+
+const DishOutData = (props) => {
+  for (let i in props.data) {
+    return (
+      <div><b>{i}:</b> {props.data[i]}</div>
+    );
+  }
+}
 
 class BlankSlate extends React.Component {
 
   state = {
-    character: null,
-    myID: 0
+    entity: null,
+    category: null,
+    number: null,
   };
   
   componentDidMount() {
-    let id = null;
-    if(this.props.index === undefined) {
-      id = this.props.match.params.id
-    } else { id = this.props.index + 1 }
+    const category = this.props.match.params.category
+    const number = this.props.match.params.number
     axios
-      .get(`https://swapi.co/api/people/${id}/`)
-      .then(response => this.setState(() => ({ character: response, myID: id-1}) ))
+      .get(`https://swapi.co/api/${category}/${number}/`)
+      .then(response => this.setState(() => ({ entity: response, number: number, category: category}) ))
       .catch(error => {console.error(error)});
   }
 
   render() {
-    if(!this.state.character) {
-      return <div>Loading character information...</div>
+    if(!this.state.entity) {
+      return <div>Loading information...</div>
     }
 
-    const { name, birth_year, gender, eye_color, hair_color, skin_color, height, mass, created, edited } = this.state.character.data;
     return(
       <div className="container">
         <div className="container__topRow">        {/* top row */}
-          <div>Star Wars</div>
-          <div>Character License</div>
+          <div>{this.state.category}</div>
+          <div>{this.state.number}</div>
         </div>
         <div className="container__middleRow">        {/* middle row */}
-          <img src={pictures[this.state.myID].picture} alt={name} />
+          {/* <img src={pictures[this.state.myID].picture} alt={name} /> */}
           <div className="container__middleRow-right">          {/* center start */}
-            <div><b>Name:</b> {name}</div>
-            <div><b>YOB:</b> {birth_year}</div>
-            <div><b>Gender:</b> {gender}</div>
-            <div><b>Eye Color:</b> {eye_color}</div>
-            <div><b>Hair Color:</b> {hair_color}</div>
-            <div><b>Skin:</b> {skin_color}</div>
-            <div><b>Height:</b> {height}</div>
-            <div><b>Mass:</b> {mass}</div>
+            <DishOutData data={this.state.entity.data} />
           </div>          {/* center end */}
-        </div>
-        <div className="container__bottomRow">        {/* bottom row */}
-          <div><b>created:</b> {created}</div>
-          <div><b>edited:</b> {edited}</div>
         </div>
       </div>
     );
